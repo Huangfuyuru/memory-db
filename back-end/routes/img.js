bfef335feb1f545b9cf97f5e83853074df084055
huangfuyuru <864134710@qq.com>
2020-04-09 15:50:15 +0800

    a

--- back-end/routes/img.js
+++ back-end/routes/img.js
@@ -5,17 +5,16 @@ const fs = require('fs');
 router.post('/',function(req,res){
     console.log("hello");
     var form = new formidable.IncomingForm();
-    form.uploadDir = "D:/database/img";
+    form.uploadDir = "/home/shared_work/img";
     form.keepExtensions = true; 
     form.maxFieldSize = 2*1024*1024;
     form.parse(req, function (error, fields, files) { 
-        // console.log(files); 
         if(error) {
             var message = {err:1, msg:"文件解析失败"};
         }
-        var a = files.file.path.split("\\");
+        var a = files.file.path.split("/");
         var b = a[a.length-1];
-        var message = {err:0, path:`http://localhost:3001/img/showimg/${b}`};
+        var message = {err:0, path:`http:148.70.223.218:3001/img/showimg/${b}`};
         res.write(JSON.stringify(message));   
         res.end();
     });
@@ -23,9 +22,11 @@ router.post('/',function(req,res){
 })
 
 router.get('/showimg/:name',function(req,res){
+    console.log('显示');
     var filename = req.params.name;
     var ext = filename.split('.')[1];
-    var img = fs.readFileSync(`D:/database/img/${filename}`);
+    var img = fs.readFileSync(`/home/shared_work/img/${filename}`);
+    
     res.writeHead(200,{
         "Content-Type":"image"+ext
     })
--- back-end/routes/imgs.js
+++ back-end/routes/imgs.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 router.post('/',function(req,res){
     var files = [];
     var form = new formidable.IncomingForm();
-    form.uploadDir = "D:/database/img";
+    form.uploadDir = "/home/shared_work/img";
     form.keepExtensions = true; 
     form.maxFieldSize = 2*1024*1024;
     form.on('file', function(field, file) {
@@ -19,13 +19,12 @@ router.post('/',function(req,res){
             var message = {err:1, msg:"文件解析失败"};
         }else{
             var newFiles = files.map(function(item){
-                var a = item.path.split("\\");
+                var a = item.path.split("/");
                 var b = a[a.length-1];
-                var message = {err:0, path:`http://localhost:3001/img/showimg/${b}`};
+                var message = {err:0, path:`http://148.70.223.218:3001/img/showimg/${b}`};
                 return message;
             })
         }
-        // console.log(newFiles)
         res.write(JSON.stringify(newFiles));   
         res.end();
     });
