089e4a7646a42630bdfea05b6341f48a9f294b22
yingzi <yingzi@example.com>
2020-05-04 11:40:18 +0800

    ying

--- back-end/routes/my/sign.js
+++ back-end/routes/my/sign.js
@@ -1,36 +1,28 @@
 const express = require('express'),
       router = express.Router(),
+      qs = require('querystring'),
+      url = require('url'),
       bodyParser = require("body-parser");
 //引入数据库
 const {userM} = require("../../database/dateMethod");
 
 var info = {};
-
 //配置bodyparser中间件
 router.use(bodyParser.urlencoded({extended:true}));
 router.use(bodyParser.json());
 
 //点击签到
-// num , 用户uid
-router.post('/',async function(req,res,next){
-    console.log(req.body);
-    // var num = req.body.num;
-    //用户的id
-    var uid = req.body.uid;
-    //控制台输出 验证
-    console.log(req.body);
-    // console.log(num);
-    console.log(uid);
-
+//用户uid
+router.get('/',async function(req,res,next){
+    var request = qs.parse(url.parse(req.url).query);
+    var uid = Number(request.uid);
     var result = await userM.changeNum(uid);
     if(result == 0){
-        info = {code:0,msg:"签到成功",data:result}
-        res.json(info)
+        var info = {code:0,msg:"签到成功",data:result}
     }else{
-        info = {code:1,msg:"签到失败",data:null};
-        res.json(info)
+        var info = {code:1,msg:"签到失败",data:null};
     }
-
+    res.json(info)
 })
 
 module.exports = router;
