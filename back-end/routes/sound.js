22cc82ff3166f934f526f7b5d641f6042d24066e
yangxindi2017 <694076236@qq.com>
2020-05-07 17:56:31 +0800

    sound

--- back-end/routes/sound.js
+++ back-end/routes/sound.js
@@ -8,7 +8,7 @@ router.post('/',function(req,res){
     var form = new formidable.IncomingForm();
     form.parse(req,function(err,fields,files){
         console.log('fields',fields);
-        console.log('files',files)
+        console.log('files',files);
     })
 
 })
