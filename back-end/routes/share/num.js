280eb667d927d67d1b96adc1ba640b19d5357089
yangxindi2017 <694076236@qq.com>
2020-05-12 18:47:04 +0800

    num

--- back-end/routes/share/num.js
+++ back-end/routes/share/num.js
@@ -22,18 +22,21 @@ router.get('/addnum', async function(req,res,next){
 
     //先查看送花的人是否有小花
     var user = await article.userM.findById(uid);
-    console.log(uid+'的小花数为'+user.num)
+    // console.log(uid+'的小花数为'+user.num)
     if(user.num>0){
-        console.log('uid',typeof(uid));
         var data = await article.articleM.reduceNumByUId(uid);
         var data1 = await article.articleM.appendNumById(auid,id);
         if(data === 1 || data1 === 1){
             info={code:1,msg:null}
         }else{
             var art= await article.articleM.findById(id);
-            var auser = await article.userM.findById(auid);
-            art.push(auser);
-            art.push(user)
+            var auser_num = await article.userM.findById(auid);
+            var user_num = await article.userM.findById(uid);
+            console.log('作者的',auser_num.num)
+            console.log('送花人的',user_num.num)
+            // art.push(auser);
+            art[0].unum = user_num.num;
+            art[0].anum = auser_num.num;
             console.log(art)
 
             info={code:0,msg:art};
