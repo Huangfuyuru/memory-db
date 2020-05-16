ef6139d0fff0f18c078044e44e97e25bf0eb24b8
yingzi <yingzi@example.com>
2020-05-16 20:29:58 +0800

    ll

--- back-end/routes/my/sign.js
+++ back-end/routes/my/sign.js
@@ -17,6 +17,7 @@ router.get('/',async function(req,res,next){
     var request = qs.parse(url.parse(req.url).query);
     var uid = Number(request.uid);
     var result = await userM.changeNum(uid);
+    console.log(uid);
     if(result == 0){
         var data1 = await userM.findById(uid);
         var message = {code:0,msg:"签到成功",data:data1}
