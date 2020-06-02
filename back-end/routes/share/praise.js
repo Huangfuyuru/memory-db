2bf53a546e5ea2e179c80c835efa980ad5ac588b
yingzi <yingzi@example.com>
2020-06-02 16:16:38 +0800

    ll

--- back-end/routes/share/praise.js
+++ back-end/routes/share/praise.js
@@ -22,28 +22,48 @@ router.get('/addpraise', async function(req,res,next){
     console.log("article_id",article_id);
     console.log("user_id",user_id);
     if(result == 0){
+        // var ifhave = await likeArticleM.findByUid(user_id);
+        // for (var j =0;j<ifhave.length ;j++){
+        //     if (ifhave[i].article_id == article_id){
+                
+        //     }
+        // }
+
+        //  //点赞字段
+        //  var zan = await likeArticleM.findByUid(user_id);
+        //  for(var i=0;i<ddd.length;i++){ 
+        //      for(var m=0;m<zan.length;m++){
+        //          if(ddd[i].id == zan[m].article_id){
+        //              ddd[i].addZan = zan[m].zan;
+        //              console.log (i);
+        //              console.log ("ddd[i].addZan",ddd[i].addZan);
+        //          }
+        //      }
+        //  }
+
+        //添加到我喜欢
         var data1 = await likeArticleM.addlikeArticle({
             article_id:article_id,
             user_id:user_id
         });
         if(data1 == 0){
              //点赞字段
-            var zan = await likeArticleM.findByUid(user_id);
-            for(var i=0;i<ddd.length;i++){ 
-                for(var m=0;m<zan.length;m++){
-                    if(ddd[i].id == zan[m].article_id){
-                        ddd[i].addZan = zan[m].zan;
-                    }
+             for(var i=0;i<ddd.length;i++){ 
+                if(ddd[i].id == article_id){
+                    ddd[i].addZan = true;
+                    console.log (i);
+                    console.log ("ddd[i].addZan",ddd[i].addZan);
                 }
             }
-            var info = {code:0,msg:"点赞且增加到我喜欢成功"}
+            var info = {code:0,msg:"点赞且增加到我喜欢成功",data:ddd}
         }else{
-            var info = {code:1,msg:"增加到我喜欢失败"};
+            var info = {code:1,msg:"增加到我喜欢失败",data:ddd};
         }
     }else{
         var info = {code:1,msg:"点赞失败"};
     }
     res.json(info);
+    console.log(info);
 })
 
 
@@ -61,22 +81,30 @@ router.get('/reducepraise', async function(req,res,next){
     if(result == 0){
         var data1 = await likeArticleM.dellikeArticleByTwo(user_id,article_id);
         if(data1 == 0){
-            var zan = await likeArticleM.findByUid(user_id);
+            //取消点赞字段
             for(var i=0;i<ddd.length;i++){ 
-                for(var m=0;m<zan.length;m++){
-                    if(ddd[i].id == zan[m].article_id){
-                        ddd[i].addZan = zan[m].zan;
-                    }
+                if(ddd[i].id == article_id){
+                    ddd[i].addZan = false;
+                    console.log (i);
+                    console.log ("ddd[i].addZan",ddd[i].addZan);
                 }
             }
-            var info = {code:0,msg:"取消点赞且删除我喜欢成功"};
+            // var zan = await likeArticleM.findByUid(user_id);
+            // for(var i=0;i<ddd.length;i++){ 
+            //     for(var m=0;m<zan.length;m++){
+            //         if(ddd[i].id == zan[m].article_id){
+            //             ddd[i].addZan = zan[m].zan;
+            //         }
+            //     }
+            // }
+            var info = {code:0,msg:"取消点赞且删除我喜欢成功",data:ddd};
         }else{
-            var info = {code:1,msg:"删除我喜欢失败"};
+            var info = {code:1,msg:"删除我喜欢失败",data:ddd};
         }
     }else{
         var info = {code:1,msg:"取消点赞失败"};
     }
     res.json(info);
-
+    console.log(info);
 })
 module.exports = router;
